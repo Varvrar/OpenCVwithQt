9a473f9760c29476d18c30561693ae821f795b16
Highlight Clark <highlightz@163.com>
2015-07-14 16:30:49 +0800

    Update Snippets.cpp

--- Snippets.cpp
+++ Snippets.cpp
@@ -6,3 +6,10 @@ duration = static_cast< double >( cv::getTickCount( ) );
 Foo( );  // The function to be tested
 duration = static_cast< double >( cv::getTickCount( ) ) - duration;
 duration /= cv::getTickFrequency( );  // The elapsed time in ms
+
+/*
+* Save image sequences(C style API)
+*/
+char imageName[64];
+sprintf( imgName, "imageBaseName_%d.jpg", nFrameNum );
+cvSaveImage( imgName, pImg );
