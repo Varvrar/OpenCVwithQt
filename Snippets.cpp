441a586d0f2fee2901aa9d5b5751d3c67559c8b4
Highlight Clark <highlightz@163.com>
2015-07-14 15:32:22 +0800

    Update Snippets.cpp

--- Snippets.cpp
+++ Snippets.cpp
@@ -1,4 +1,6 @@
-The usual pattern to use in order to obtain the computational time of a given function (or portion of code):
+/*
+* The usual pattern to use in order to obtain the computational time of a given function (or portion of code).
+*/
 double duration;
 duration = static_cast< double >( cv::getTickCount( ) );
 Foo( );  // The function to be tested
