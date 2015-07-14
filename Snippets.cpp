9d7e47a236117006e8ed16d7c00b0b8f93c12830
Highlight Clark <highlightz@163.com>
2015-07-14 15:31:31 +0800

    Create Snippets.cpp

---
+++ Snippets.cpp
@@ -0,0 +1,6 @@
+The usual pattern to use in order to obtain the computational time of a given function (or portion of code):
+double duration;
+duration = static_cast< double >( cv::getTickCount( ) );
+Foo( );  // The function to be tested
+duration = static_cast< double >( cv::getTickCount( ) ) - duration;
+duration /= cv::getTickFrequency( );  // The elapsed time in ms
